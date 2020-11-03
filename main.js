a35a600bd2a1ce594738109fd15f5ee1b29464f0
Nickz <58891726+Nickz-dev@users.noreply.github.com>
2020-11-04 05:19:17 +0700

    initial commit

---
+++ main.js
@@ -0,0 +1,108 @@
+//Movement Animation to happen
+const card = document.querySelector(".card_2");
+const container = document.querySelector(".container");
+
+//Moving Animation Event
+container.addEventListener("mousemove", (e) => {
+    let xAxis = (window.innerWidth / 2 - e.pageX) / 150;
+    let yAxis = (window.innerHeight / 2 - e.pageY) / 150;
+    card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
+});
+//Animate In
+container.addEventListener("mouseenter", (e) => {
+    card.style.transition = "none";
+});
+//Animate Out
+container.addEventListener("mouseleave", (e) => {
+    card.style.transition = "all 0.5s ease";
+    card.style.transform = `rotateY(0deg) rotateX(0deg)`;
+});
+
+
+let cats = document.querySelector('#cats')
+let card_2 = document.querySelector('#card_2')
+let btnCats = document.querySelector('#btnCats')
+btnCats.addEventListener('click', getFactFetch)
+
+function getFactFetch() {
+    fetch('https://api.thecatapi.com/v1/images/search')
+        .then((response) => response.json())
+        .then((data) => {
+            card_2.style.display = 'none'
+            cats.style.display = 'block'
+            let catsImgUrl = data[0].url
+            let catsImgEl = document.createElement('img')
+            catsImgEl.setAttribute('src', `${catsImgUrl}`)
+            catsImgEl.classList.add("showcase")
+            cats.appendChild(catsImgEl)
+
+        })
+        .catch(err => console.log(err))
+}
+
+class TypeWriter {
+    constructor(txtElement, words, wait = 3000) {
+        this.txtElement = txtElement;
+        this.words = words;
+        this.txt = '';
+        this.wordIndex = 0;
+        this.wait = parseInt(wait, 10);
+        this.type();
+        this.isDeleting = false;
+    }
+
+    type () {
+// Current index of word
+        const current = this.wordIndex % this.words.length;
+        // Get full text of current word
+        const fullTxt = this.words[current];
+
+        // Check if deleting
+        if(this.isDeleting) {
+            // Remove char
+            this.txt = fullTxt.substring(0, this.txt.length - 1);
+        } else {
+            // Add char
+            this.txt = fullTxt.substring(0, this.txt.length + 1);
+        }
+
+        // Insert txt into element
+        this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
+
+        // Initial Type Speed
+        let typeSpeed = 50;
+
+        if(this.isDeleting) {
+            typeSpeed /= 2;
+        }
+
+        // If word is complete
+        if(!this.isDeleting && this.txt === fullTxt) {
+            // Make pause at end
+            typeSpeed = this.wait;
+            // Set delete to true
+            this.isDeleting = true;
+        } else if(this.isDeleting && this.txt === '') {
+            this.isDeleting = false;
+            // Move to next word
+            this.wordIndex++;
+            // Pause before start typing
+            typeSpeed = 300;
+        }
+
+        setTimeout(() => this.type(), typeSpeed);
+    }
+}
+
+// Init On DOM Load
+document.addEventListener('DOMContentLoaded', init)
+
+//Init App
+function init() {
+    const txtElement = document.querySelector('.txt-type');
+    const words = JSON.parse(txtElement.getAttribute('data-words'));
+    const wait = txtElement.getAttribute('data-wait');
+    // Init TypeWriter
+    new TypeWriter(txtElement, words, wait);
+}
+
