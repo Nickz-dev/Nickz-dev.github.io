e039e66331bc966204b16b6b68a5c8140249c31c
Nickz <58891726+Nickz-dev@users.noreply.github.com>
2020-11-11 20:27:14 +0700

    initial commit

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ window.onload = function () {
     window.setTimeout(function () {
         document.body.classList.add('loaded');
         document.body.classList.remove('loaded_hiding');
-    }, 3500);
+    }, 1500);
 }
 
 
